e717095cd1a250b9891afe07bc1c6b759075c3fc
flkn-37 <fabrj9@gmail.com>
2021-10-07 14:17:56 -0300

    Adicionado app de séries

---
+++ Enum/Genero.cs
@@ -0,0 +1,27 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+         
+        Acao = 1,
+
+        Aventura = 2,
+
+        Comedia = 3,
+
+        Documentarios = 4,
+
+        Drama = 5,
+
+        Fantasia = 6,
+
+        Romance = 10,
+
+        Suspense = 15,
+
+        Terror = 18,
+    }
+
+}
+
+    
